f71ca87c51af1ad494bef5d9b17ba178a64af85e
srujan-j24 <srujanj2021@gmail.com>
2024-05-20 17:37:16 +0530

    sfa

--- frontend/src/components/templates/LoginPage/LoginPage.jsx
+++ frontend/src/components/templates/LoginPage/LoginPage.jsx
@@ -5,7 +5,7 @@ const LoginPage = () => {
     return (
         <>
             <div className='h-svh w-full flex justify-center items-center bg-background' style={{backgroundImage: `url(${backgroundSvg})`, backgroundPosition: 'center'}}>
-                <div className='sm:w-96 mx-8 w-full min-w-80 max-w-96'>
+                <div className=' mx-8 w-full min-w-80 max-w-96'>
                     <LoginForm name='Rolls Corner'></LoginForm>
                 </div>
             </div>
