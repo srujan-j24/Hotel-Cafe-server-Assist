1a9bf41cdb716c0a96847a60fd06f0b82881ed9a
srujan-j24 <srujanj2021@gmail.com>
2024-05-31 21:04:52 +0530

    asf

--- frontend/src/components/templates/ServePage/ServePage.jsx
+++ frontend/src/components/templates/ServePage/ServePage.jsx
@@ -8,8 +8,7 @@ function ServePage() {
     const tables  = useSelector(state => state.table.tables);
     return (
         <>  
-            {/* <ScrollArea className="h-dvh pt-12"> */}
-            <main className="min-h-svh">
+            <ScrollArea className="h-dvh pt-12">
                 <div className="grid auto-fit-20 place-items-center gap-y-6 py-6 px-4 gap-x-4" id='tables-grid'>
                     {
                         tables.map((tableInfo, index)=> (
@@ -18,8 +17,7 @@ function ServePage() {
                     }
                 </div>
                 <TableDialog ref={dialogRef}></TableDialog>
-            </main>
-            {/* </ScrollArea> */}
+            </ScrollArea>
         </>
     );
 }
