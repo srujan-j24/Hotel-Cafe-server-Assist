cde871812402ce101836bf642b40023392575b08
srujan-j24 <srujanj2021@gmail.com>
2024-05-31 21:56:31 +0530

    f

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div className='min-h-dvh relative' style={{backgroundImage: `url(${backgroundSvg})`, backgroundPosition: 'center'}}>
+      <div className='h-screen relative' style={{backgroundImage: `url(${backgroundSvg})`, backgroundPosition: 'center'}}>
         <Header></Header>
         <Outlet></Outlet>
       </div>
