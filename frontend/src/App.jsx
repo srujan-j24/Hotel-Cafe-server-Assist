fb2e7124b6a88298528d237d47138a8ce5814ab8
srujan-j24 <srujanj2021@gmail.com>
2024-06-01 11:37:35 +0530

    scroll hadled

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,14 @@ import Header from './components/templates/Header'
 import { Outlet } from 'react-router-dom'
 import backgroundSvg from './assets/background.svg'
 import { ScrollArea } from './components/ui/scroll-area'
+import { useRef } from 'react'
 function App() {
-
+  const scrollRef = useRef(null)
   return (
     <>
-      <div className='h-screen relative' style={{backgroundImage: `url(${backgroundSvg})`, backgroundPosition: 'center'}}>
+      <div className='h-screen relative' ref={scrollRef} style={{backgroundImage: `url(${backgroundSvg})`, backgroundPosition: 'center'}}>
         <Header></Header>
-        <Outlet></Outlet>
+        <Outlet scrollRef={scrollRef}></Outlet>
       </div>
     </>
   )
--- frontend/src/components/templates/ServePage/ServePage.jsx
+++ frontend/src/components/templates/ServePage/ServePage.jsx
@@ -1,14 +1,20 @@
+/* eslint-disable react/prop-types */
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { useRef } from "react";
 import { useSelector } from "react-redux";
 import TableCard from "./TableCard";
 import TableDialog from "./TableDialog";
-function ServePage() {
+function ServePage({scrollRef}) {
     const dialogRef = useRef(null);
     const tables  = useSelector(state => state.table.tables);
+    const handleScroll = (e) =>{
+        if(scrollRef.current){
+            scrollRef.current.scrollTop = e.target.scrollTop;
+        }
+    }
     return (
         <>  
-            <ScrollArea className="h-dvh pt-12">
+            <ScrollArea className="h-dvh pt-12" onScroll={handleScroll}>
                 <div className="grid auto-fit-20 place-items-center gap-y-6 py-6 px-4 gap-x-4" id='tables-grid'>
                     {
                         tables.map((tableInfo, index)=> (
